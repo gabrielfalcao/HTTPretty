9fb857ec00d47c7b83994c8eb6e41c9bcea44319
Guillaume Gauvrit <guillaume@gandi.net>
2013-07-26 16:01:58 +0200

    Fix tests
    Functional test change the content of POTENTIAL_HTTP_PORTS
    but don't reset it. Ignore those changes.

--- tests/unit/test_httpretty.py
+++ tests/unit/test_httpretty.py
@@ -61,6 +61,7 @@ def test_httpretty_should_raise_on_socket_send_when_uri_registered():
     HTTPretty.enable()
 
     defaults = core.POTENTIAL_HTTP_PORTS[:]
+    core.POTENTIAL_HTTP_PORTS = [80, 443]
     HTTPretty.register_uri(HTTPretty.GET,
                            'http://127.0.0.1:5000')
     expect(core.POTENTIAL_HTTP_PORTS).to.be.equal([80, 443, 5000])
