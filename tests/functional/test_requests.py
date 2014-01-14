be9ccb301f95fde988d45ac6abb2c4de4b6871ee
Jamie Lennox <jamielennox@redhat.com>
2014-01-14 11:54:17 +1000

    Free version requirements
    Tests should be run against the latest versions of libraries. Initially
    there was going to be a lower bound on requirements but it is unlikely
    that a situation will arise where a version is too out of date and can
    be added back then rather than having arbitrary minimums.

--- tests/functional/test_requests.py
+++ tests/functional/test_requests.py
@@ -36,6 +36,7 @@ from httpretty import HTTPretty, httprettified
 from httpretty.core import decode_utf8
 
 from base import FIXTURE_FILE, use_tornado_server
+from tornado import version as tornado_version
 
 try:
     xrange = xrange
@@ -698,7 +699,7 @@ def test_recording_calls():
     response['response'].should.have.key("status").being.equal(200)
     response['response'].should.have.key("body").being.an(unicode)
     response['response'].should.have.key("headers").being.a(dict)
-    response['response']["headers"].should.have.key("server").being.equal("TornadoServer/2.4")
+    response['response']["headers"].should.have.key("server").being.equal("TornadoServer/" + tornado_version)
 
 
 def test_playing_calls():
