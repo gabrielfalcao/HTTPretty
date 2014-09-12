78c74a8e3348f5b5d4e76d859c75970c37faac71
smartkiwi <vladimir.vladimirov@magnetic.com>
2014-09-12 13:33:27 -0400

    Update __init__.py

--- httpretty/__init__.py
+++ httpretty/__init__.py
@@ -25,7 +25,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 from __future__ import unicode_literals
 
-__version__ = version = '0.8.3'
+__version__ = version = '0.8.3.1'
 
 from .core import httpretty, httprettified
 from .errors import HTTPrettyError
