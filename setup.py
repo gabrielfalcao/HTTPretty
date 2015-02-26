f49d8a5d82cd90032508c09d689b540c2289523a
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-02-26 11:42:48 +0100

    use correct encoding

--- setup.py
+++ setup.py
@@ -24,29 +24,16 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-import ast
-import io
 import os
 import re
+import codecs
 from setuptools import setup, find_packages
-
-
-class VersionFinder(ast.NodeVisitor):
-
-    def __init__(self):
-        self.version = None
-
-    def visit_Assign(self, node):
-        if node.targets[0].id == '__version__':
-            self.version = node.value.s
 
 
 def read_version():
     """Read version from httpretty/version.py without loading any files"""
-    finder = VersionFinder()
-    finder.visit(
-        ast.parse(local_file('httpretty', '__init__.py').encode('utf-8')))
-    return finder.version
+    contents = local_file('httpretty', '__init__.py')
+    return re.compile(r".*__version__ = version = '(.*?)'", re.S).match(contents).group(1)
 
 
 def parse_requirements(path):
@@ -57,7 +44,7 @@ def parse_requirements(path):
     function properly.
     """
     try:
-        requirements = [req.strip() for req in local_file(path).splitlines()]
+        requirements = (line.strip() for line in local_file(path).splitlines())
     except IOError:
         raise RuntimeError("Couldn't find the `requirements/use.txt' file :(")
 
@@ -76,40 +63,41 @@ def parse_requirements(path):
     return pkgs, links
 
 
-local_file = lambda *f: \
-    io.open(
-        os.path.join(os.path.dirname(__file__), *f), encoding='utf-8').read()
+def local_file(*f):
+    with codecs.open(os.path.join(os.path.dirname(__file__), *f), encoding='utf-8') as fd:
+        return fd.read()
 
 
 install_requires, dependency_links = \
     parse_requirements('requirements/use.txt')
 
 
-setup(name='httpretty',
-      version=read_version(),
-      description='HTTP client mock for Python',
-      long_description=local_file('README.rst'),
-      author='Gabriel Falcao',
-      author_email='gabriel@nacaolivre.org',
-      url='http://github.com/gabrielfalcao/httpretty',
-      zip_safe=False,
-      packages=find_packages(exclude=['*tests*']),
-      tests_require=parse_requirements('test-requirements.txt'),
-      install_requires=install_requires,
-      dependency_links=dependency_links,
-      license='MIT',
-      test_suite='nose.collector',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python',
-          'Topic :: Internet :: WWW/HTTP',
-          'Topic :: Software Development :: Testing'
-      ],
+setup(
+    name='httpretty',
+    version=read_version(),
+    description='HTTP client mock for Python',
+    long_description=local_file('README.rst'),
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://github.com/gabrielfalcao/httpretty',
+    zip_safe=False,
+    packages=find_packages(exclude=['*tests*']),
+    tests_require=parse_requirements('requirements/test.txt')[0],
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license='MIT',
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Testing'
+    ],
 )
