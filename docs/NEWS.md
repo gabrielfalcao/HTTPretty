2471cd11fe14d8a9384abc53fc1fe0f1b1daecb7
Gabriel Falcão <gabriel@nacaolivre.org>
2015-03-11 12:48:41 -0400

    New release: 0.8.8

--- docs/NEWS.md
+++ docs/NEWS.md
@@ -1,6 +1,6 @@
 # Release Notes
 
-## 0.8.7 (current)
+## 0.8.8 (current)
 
 Improvements:
 
--- httpretty/__init__.py
+++ httpretty/__init__.py
@@ -25,7 +25,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 from __future__ import unicode_literals
 
-__version__ = version = '0.8.7'
+__version__ = version = '0.8.8'
 
 from .core import httpretty, httprettified
 from .errors import HTTPrettyError, UnmockedError
