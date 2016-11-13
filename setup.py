c6bc0d739209ec79b677bce70ebcfa782d780c9f
Mantas <sirexas@gmail.com>
2016-11-13 22:08:20 +0200

    Add python 3 to setup.py classifiers
    Tools like requires.io recongnise Python 3 by reading classifiers.
    
    Added only python 3.5 since it is tested with tox.

--- setup.py
+++ setup.py
@@ -109,6 +109,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Testing'
