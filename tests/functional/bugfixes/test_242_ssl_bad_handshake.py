56feb82ed68df398d8034c0fb84572668de3df7f
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-11 04:02:44 +0100

    move tests that reproduce github issues into their own folder

---
+++ tests/functional/bugfixes/test_242_ssl_bad_handshake.py
@@ -0,0 +1,16 @@
+import httpretty
+import requests
+
+
+@httpretty.activate
+def test_test_ssl_bad_handshake():
+    # Reproduces https://github.com/gabrielfalcao/HTTPretty/issues/242
+
+    url_http = 'http://httpbin.org/status/200'
+    url_https = 'https://github.com/gabrielfalcao/HTTPretty'
+
+    httpretty.register_uri(httpretty.GET, url_http, body='insecure')
+    httpretty.register_uri(httpretty.GET, url_https, body='encrypted')
+
+    requests.get(url_http).text.to.equal('insecure')
+    requests.get(url_https).text.to.equal('encrypted')
