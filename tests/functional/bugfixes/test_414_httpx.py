2f9a12b6bf305fb7704d2664d606fc45b6754d79
Gabriel Falcão <gabriel@nacaolivre.org>
2021-05-14 01:11:43 +0200

    implement support to httpx library
    closes #414

---
+++ tests/functional/bugfixes/test_414_httpx.py
@@ -0,0 +1,12 @@
+import httpretty
+import httpx
+from sure import expect
+
+@httpretty.activate(verbose=True, allow_net_connect=False)
+def test_httpx():
+    httpretty.register_uri(httpretty.GET, "https://blog.falcao.it/",
+                           body="Posts")
+
+    response = httpx.get('https://blog.falcao.it')
+
+    expect(response.text).to.equal("Posts")
